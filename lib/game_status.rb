a153ae72632e838d013fb36bc1f470263c3dc8aa
vincent servio <vincentservio@gmail.com>
2019-08-25 18:03:51 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,6 +1,20 @@
 # Helper Method
+
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
-# Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [6,4,2]
+  ]
+  def won? (board)
+    WIN_COMBINATIONS.detect do |combo|
+      position_taken? (board, combo[0]) && board [combo[0]]  == board [combo[1]] && board[combo[1]] == board [combo [2]]
+    end
+    end 
